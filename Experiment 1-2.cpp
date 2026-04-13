21c7659cad7a77b6b51e85647dfe8da6f5bd30a5
ASUS <ASUS@LAPTOP-6DT6U7BF>
2026-04-13 15:00:03 +0800

    2026/4/13 下午 剩下两题的一些基本思路

---
+++ Experiment 1-2.cpp
@@ -0,0 +1,68 @@
+/*
+    实验一 T2：纸牌游戏
+    对按编号排列的纸牌按倍数规律进行翻面操作，
+    制定状态值：正面朝上为1 反之为0
+    注意到在不计入1的情况下，
+    只有完全平方数的因数为偶数（因数有他本身）
+    才能正面朝上
+    最终输出正面朝上的纸牌编号及数量，
+    通过本题掌握顺序表的使用和状态模拟的方法。
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int ElemType;
+
+typedef struct {
+    ElemType* elem;    // 空间基地址，空间存放纸牌正反面状态值,正/反分别用1/0表示
+    int length;        // 存放纸牌数
+    int listsize;      // 存放空间的容量
+} SqList;
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    SqList L;
+    L.length = n;
+    L.listsize = n + 1;   // 0号位置闲置，使用1~n
+    L.elem = (ElemType*)malloc((n + 1) * sizeof(ElemType));
+
+    if (L.elem == NULL) {
+        return 0;
+    }
+
+    // 初始化：1~n号牌都正面朝上，记为1
+    for (int i = 1; i <= n; i++) {
+        L.elem[i] = 1;
+    }
+
+    // 模拟翻牌过程
+    // 从第2张牌开始，以2为基数；再以3为基数……直到n
+    for (int i = 2; i <= n; i++) {
+        for (int j = i; j <= n; j += i) {
+            if (L.elem[j] == 1) {
+                L.elem[j] = 0;
+            }
+            else {
+                L.elem[j] = 1;
+            }
+        }
+    }
+
+    // 输出正面朝上的牌编号
+    int count = 0;
+    for (int i = 1; i <= n; i++) {
+        if (L.elem[i] == 1) {
+            printf("%d ", i);
+            count++;
+        }
+    }
+
+    // 输出正面朝上的纸牌张数
+    printf("\n%d", count);
+
+    free(L.elem);
+    return 0;
+}
---
+++ Experiment 1-3.cpp
@@ -0,0 +1,111 @@
+/*
+    实验一 第三题：火车排序
+    利用栈和队列将两条从大到小排列的车厢序列合并为一条从小到大的有序序列，
+    依次对栈顶元素进行比较，小的先入队
+    队列的特性是先进先出
+    所有元素入队完再依次出队，就构成了从小到大的有序序列
+*/
+#include <stdio.h>
+#define MAXSIZE 1000
+
+typedef struct {
+    int data[MAXSIZE];
+    int top;
+} Stack;
+
+typedef struct {
+    int data[MAXSIZE];
+    int front;
+    int rear;
+} Queue;
+
+// 这里用结构体指针成员访问符->
+// 因为s不是变量本身，而是指向变量的指针
+void InitStack(Stack* s) {
+    s->top = -1;
+}
+
+int StackEmpty(Stack* s) {
+    return s->top == -1;
+    // 若 == -1 返回真 没读到返回false
+    // OJ平台用的是C语言系统，没有bool
+}
+
+void Push(Stack* s, int x) {
+    s->data[++s->top] = x;
+}
+
+int Pop(Stack* s) {
+    return s->data[s->top--];
+}
+
+int GetTop(Stack* s) {
+    return s->data[s->top];
+}
+
+void InitQueue(Queue* q) {
+    q->front = q->rear = 0;
+}
+
+int QueueEmpty(Queue* q) {
+    return q->front == q->rear;
+}
+
+// 数据入队
+void EnQueue(Queue* q, int x) {
+    q->data[q->rear++] = x;
+}
+
+// 数据出队
+int DeQueue(Queue* q) {
+    return q->data[q->front++];
+}
+
+int main() {
+    Stack s1, s2;
+    Queue q;
+    InitStack(&s1);
+    InitStack(&s2);
+    InitQueue(&q);
+
+    int x;
+    char ch;
+
+    // 读第一个栈
+    while (scanf("%d", &x) == 1) { //成功读到数返回1 没读到返回0
+        Push(&s1, x);
+        ch = getchar();
+        if (ch == '\n')
+            break;
+    }
+
+    // 读第二个栈
+    while (scanf("%d", &x) == 1) {
+        Push(&s2, x);
+        ch = getchar();
+        if (ch == '\n')
+            break;
+    }
+
+    // 进行两个栈元素的合并
+    // 判空函数是返回1的 这个while进行循环的条件是 两者都不是空栈
+    while (!StackEmpty(&s1) && !StackEmpty(&s2)) {
+        if (GetTop(&s1) < GetTop(&s2))
+            EnQueue(&q, Pop(&s1));
+        else
+            EnQueue(&q, Pop(&s2));
+    }
+
+    // 当两个栈对应元素比较完后还有剩余
+    while (!StackEmpty(&s1))
+        EnQueue(&q, Pop(&s1));
+
+    while (!StackEmpty(&s2))
+        EnQueue(&q, Pop(&s2));
+
+    // 排好的从小到大的元素依次出队
+    while (!QueueEmpty(&q))
+        printf("%d ", DeQueue(&q));
+
+    return 0;
+}
